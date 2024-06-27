268b7911057b1ebaa4e20e254e01bdd6c266ccf7
HEMA ESTHER SHAH <166860660+HemaEsther@users.noreply.github.com>
2024-06-27 20:48:31 +0530

    Add files via upload

---
+++ News/script.js
@@ -0,0 +1,78 @@
+const api_key = '5a2036cbea474258b2e3678914a61475';
+const url = 'https://newsapi.org/v2/everything?q=';
+
+window.addEventListener("load", () => {
+    return fetchNews("India");
+});
+
+async function fetchNews(query) {
+    try {
+        const response = await fetch(`${url}${query}&apiKey=${api_key}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched data:', data); // Log the fetched data
+        if (!data.articles) {
+            throw new Error('No articles found in the response.');
+        }
+        bindData(data.articles);
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
+}
+
+function bindData(articles) {
+    const card_container = document.querySelector('#card-container');
+    const template = document.querySelector('#template-news-card');
+
+    card_container.innerHTML = '';
+    articles.forEach((article) => {
+        if (!article.urlToImage) return; // articles that do not have image do not show
+        const cardClone = template.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        card_container.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    });
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener('click', () => {
+        window.open(article.url, '_blank');
+    });
+}
+
+let currSelectedNav = null;
+
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navitem = document.getElementById(id);
+    currSelectedNav?.classList.remove('active');
+    currSelectedNav = navitem;
+    currSelectedNav.classList.add('active');
+}
+
+const searchButton = document.getElementById('news-btn');
+const searchText = document.getElementById('news-input');
+searchButton.addEventListener('click', () => {
+    const query = searchText.value;
+    if (!query) {
+        return;
+    }
+    fetchNews(query);
+    currSelectedNav?.classList.remove('active');
+    currSelectedNav = null;
+});
